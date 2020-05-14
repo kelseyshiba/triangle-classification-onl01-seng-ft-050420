4bd505cf7e3a123fe929d0fb04d57240bc1a2bc4
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-14 20:09:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,8 +11,7 @@ class Triangle
     if side1 <= 0 || side2 <= 0 || side3 <= 0 
       raise TriangleError
     end
-    
-    if (side1 == side2 && side2 == side3 && side1 > 0 && side2 > 0 && side3 > 0)
+    if (side1 == side2 && side2 == side3)
       return :equilateral
     elsif (side1 == side2 || side2 == side3 || side3 == side1 && side1 > 0 && side2 > 0 && side3 > 0)
       return :isosceles
