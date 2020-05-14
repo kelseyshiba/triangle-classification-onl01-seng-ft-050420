0d47b97211ad7a9997e96f303056a800e4d0aa79
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-14 20:10:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
   
   def kind
-    if side1 <= 0 || side2 <= 0 || side3 <= 0 
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0)
       raise TriangleError
     end
     if (side1 == side2 && side2 == side3)
