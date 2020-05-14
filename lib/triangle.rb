a0e0dbc4dadbaeadd7174dfce2d5a6bda88ce4d5
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-14 19:36:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,7 +26,6 @@ class Triangle
   
   end
   
-end
 #side > 0 all of these 
 #equilateral = side1 = side2 = side3 
 #isosceles = side1 = side 2 | side 2 = side 3 | side 3 = side1
@@ -34,10 +33,4 @@ end
   #side1 + side 2 > side3
   #side 2 + side 3 > side1
   #side 1 + side 3 > side 2
-
-
-
-
-
-
 end
