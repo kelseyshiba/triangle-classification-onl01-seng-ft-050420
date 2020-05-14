6526bbe05065cfb1fcc987468583a1d68be16f91
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-14 19:41:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,11 +8,11 @@ class Triangle
   end
   
   def kind
-    if (side1 == side2 && side2 == side3)
+    if (side1 == side2 && side2 == side3 && side1 > 0 && side2 > 0 && side3 > 0)
       return :equilateral
-    elsif (side1 == side2 || side2 == side3 || side3 == side1)
+    elsif (side1 == side2 || side2 == side3 || side3 == side1 side1 > 0 && side2 > 0 && side3 > 0)
       return :iscoceles
-    elsif (side1 + side2 > side3 || side2 + side3 > side1 || side1 + side3 > side2)
+    elsif (side1 + side2 > side3 || side2 + side3 > side1 || side1 + side3 > side2 side1 > 0 && side2 > 0 && side3 > 0)
       return :scalene
     else  
       begin 
